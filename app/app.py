365e4cb7989b1a9cffcc65496976faf91bb7e07d
EC2 Default User <ec2-user@ip-10-1-1-153.ec2.internal>
2023-11-06 01:26:12 +0000

    Refactoring app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World 2!'
 
-if __name__ == '__main__':
-    app.run(debug=False, port=5000, host='0.0.0.0')  
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)    
